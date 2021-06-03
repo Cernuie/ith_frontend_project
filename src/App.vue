607e59cb228676b8d6819aeecf7f40badbef01ab
Alexander Nguyen <cernvii@gmail.com>
2021-06-03 01:15:50 -0400

    working map shows up

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Map/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Map from './components/Map.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Map
   }
 }
 </script>
---
+++ src/components/Map.vue
@@ -0,0 +1,24 @@
+<template>
+  <GoogleMap
+  api-key="AIzaSyD3UrjtZOyQfkcx10D9R0J3kiVlIzBO-Ac"
+  style="width: 100%; height: 500px"
+  :center="center"
+  :zoom="10"
+  >
+    <Marker :options="{ position: center }" />
+  </GoogleMap>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { GoogleMap, Marker } from 'vue3-google-map'
+
+export default defineComponent({
+  components: { GoogleMap, Marker },
+  setup() {
+    const center = { lat: 40.689247, lng: -74.044502 }
+
+    return { center }
+  },
+})
+</script>
