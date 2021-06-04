d690929729982713777626e352988736476e0d38
Alexander Nguyen <cernvii@gmail.com>
2021-06-04 04:31:41 -0400

    working info windows

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -3,28 +3,13 @@
     <br>
     <h1>Vue Google Maps</h1>
 
-    <gmap-map
-      :center="center"
-      :zoom="10"
-      style="width:100%;  height: 480px;"      
-    >
-      <gmap-marker
-        :key="index"
-        v-for="(m, index) in markers"
-        :position="m"
-        @click="center=m"
-      ></gmap-marker>
-
-      <gmap-info-window
-        :options="infoOptions"
-        :position="infoWindowPos"
-        :opened="infoWinOpen"
-        @closeclick="infoWinOpen=false"
-      >
-        <div v-html="infoContent"></div>
+   <gmap-map :center="center" :zoom="10" style="width: 100%; height: 500px">
+      <gmap-info-window :options="infoOptions" :position="infoWindowPos" :opened="infoWinOpen" @closeclick="infoWinOpen=false">
+        {{infoContent}}
       </gmap-info-window>
-    </gmap-map>
 
+      <gmap-marker :key="i" v-for="(m,i) in markers" :position="m.position" :clickable="true" @click="toggleInfoWindow(m,i)"></gmap-marker>
+    </gmap-map>
   </div>
 </template>
 
@@ -34,15 +19,30 @@ export default {
   data() {
     return {
       center: { lat: 43.6532, lng: -79.3832 },
-      markers: [],
+      markers: [
+          {
+              position: {
+              lat: 43.6532, lng: -79.3832
+            },
+              infoText:'Marker 1'
+          },
+          {
+              position:{lat: 43.6532,
+              lng: -79.4832},
+              infoText:'Marker 2'
+          },
+          {
+              position:{lat: 43.7532,
+              lng: -79.5832},
+              infoText:'Marker 3'
+          }
+      ],
       currentPlace: null,
       infoContent: '',
-        infoWindowPos: {
-          lat: 0,
-          lng: 0
-        },
-        infoWinOpen: false,
-        currentMidx: null,
+      infoWindowPos: null,
+      infoWinOpen: false,
+      currentMidx: null,
+
         //optional: offset infowindow so it visually sits nicely on top of our marker
         infoOptions: {
           pixelOffset: {
@@ -52,86 +52,22 @@ export default {
         },
     };
   },
+  methods:{
+     toggleInfoWindow: function(marker, idx) {
 
-  mounted() {
-    this.geolocate();
-  },
+            this.infoWindowPos = marker.position;
+            this.infoContent = marker.infoText;
 
-  methods: {
-    setPlace(place) {
-      this.currentPlace = place;
-    },
-
-    
-    geolocate: function() {
-
-        navigator.geolocation.getCurrentPosition(position => {
-          this.center = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
-        });
-
-        this.markers = [
-          {
-              lat: 43.7532,
-              lng: -79.4832,
-              name: 'A',
-              description: 'A',
-          },
-          {
-              lat: 43.6532,
-              lng: -79.4832,
-              name: 'B',
-              description: 'B', 
-          },
-          {
-              lat: 43.7532,
-              lng: -79.5832,
-              name: 'C',
-              description: 'C'
-          }
-      ];
-
-    },
-
-    toggleInfoWindow: function (marker, idx) {
-        this.infoWindowPos = marker.position;
-        this.infoContent = this.getInfoWindowContent(marker);
-        
-
-        //check if its the same marker that was selected if yes toggle
-        if (this.currentMidx == idx) {
-          this.infoWinOpen = !this.infoWinOpen;
+            //check if its the same marker that was selected if yes toggle
+            if (this.currentMidx == idx) {
+              this.infoWinOpen = !this.infoWinOpen;
+            }
+            //if different marker set infowindow to open and reset current marker index
+            else {
+              this.infoWinOpen = true;
+              this.currentMidx = idx;
         }
-        //if different marker set infowindow to open and reset current marker index
-        else {
-          this.infoWinOpen = true;
-          this.currentMidx = idx;
-        }
-      },
-    
-    getInfoWindowContent: function (marker) {
-        return (`<div class="card">
-  <div class="card-image">
-    <figure class="image is-4by3">
-      <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-    </figure>
-  </div>
-  <div class="card-content">
-    <div class="media">
-      <div class="media-content">
-        <p class="title is-4">${marker.name}</p>
-      </div>
-    </div>
-    <div class="content">
-      ${marker.description}
-      <br>
-      <time datetime="2016-1-1">${marker.date_build}</time>
-    </div>
-  </div>
-</div>`);
       },
   }
-};
+}
 </script>
